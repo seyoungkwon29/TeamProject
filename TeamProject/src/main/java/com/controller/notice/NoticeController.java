3a4ce7e6aab7a16c47b86e90a6dea72e5c30c55c
namyesol <studhaza@naver.com>
2023-10-15 23:51:29 +0900

    서비스 레이어 트랜잭션 적용

--- TeamProject/src/main/java/com/controller/notice/NoticeController.java
+++ TeamProject/src/main/java/com/controller/notice/NoticeController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
