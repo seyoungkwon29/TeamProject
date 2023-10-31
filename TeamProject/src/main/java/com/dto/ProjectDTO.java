ab81518bab53f894c59cabace70222a1f6a29405
seyoungkwon <seyoungkwon29@gmail.com>
2023-10-31 11:11:28 +0900

    commit

--- TeamProject/src/main/java/com/dto/ProjectDTO.java
+++ TeamProject/src/main/java/com/dto/ProjectDTO.java
@@ -1,23 +1,26 @@
 package com.dto;
 
+import java.util.List;
+
 import org.apache.ibatis.type.Alias;
 
 @Alias("ProjectDTO")
 public class ProjectDTO {
 	private int project_num;
-	private String project_title;
-	private String status;
-	private String start_date;
-	private String due_date;
-	private String project_manager;
-	private int member_num;
-	
+    private String project_title;
+    private String status;
+    private String start_date;
+    private String due_date;
+    private String project_manager;
+    private int member_num;
+    private List<MemberDTO> member_list;
+    
 	public ProjectDTO() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-	
 	public ProjectDTO(int project_num, String project_title, String status, String start_date, String due_date,
-			String project_manager, int member_num) {
+			String project_manager, int member_num, List<MemberDTO> member_list) {
 		super();
 		this.project_num = project_num;
 		this.project_title = project_title;
@@ -26,8 +29,8 @@ public class ProjectDTO {
 		this.due_date = due_date;
 		this.project_manager = project_manager;
 		this.member_num = member_num;
+		this.member_list = member_list;
 	}
-	
 	public int getProject_num() {
 		return project_num;
 	}
@@ -70,11 +73,19 @@ public class ProjectDTO {
 	public void setMember_num(int member_num) {
 		this.member_num = member_num;
 	}
+	public List<MemberDTO> getMember_list() {
+		return member_list;
+	}
+	public void setMember_list(List<MemberDTO> member_list) {
+		this.member_list = member_list;
+	}
 
 	@Override
 	public String toString() {
 		return "ProjectDTO [project_num=" + project_num + ", project_title=" + project_title + ", status=" + status
 				+ ", start_date=" + start_date + ", due_date=" + due_date + ", project_manager=" + project_manager
-				+ ", member_num=" + member_num + "]";
+				+ ", member_num=" + member_num + ", member_list=" + member_list + "]";
 	}
+	
+    
 }
